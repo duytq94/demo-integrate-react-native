fbd4aa100a9187c198ae44a5e96ae03282f37102
duytq94 <tranquangduy7994@gmail.com>
2021-08-15 12:08:05 +0700

    update react-native version & fix can't run on android

--- android/app/src/main/java/com/example/demointegratern/TestConnectNativeModule.kt
+++ android/app/src/main/java/com/example/demointegratern/TestConnectNativeModule.kt
@@ -8,35 +8,31 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 
 class TestConnectNativeModule internal constructor(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
-    override fun getName(): String {
-        return "TestConnectNative"
-    }
+  override fun getName(): String {
+    return "TestConnectNative"
+  }
 
-    @ReactMethod
-    fun sendMessageToNative(rnMessage: String?) {
-        Log.d("This log is from java", rnMessage)
-    }
+  @ReactMethod
+  fun sendMessageToNative(rnMessage: String?) {
+    Log.d("This log is from java", rnMessage ?: "")
+  }
 
-    @ReactMethod
-    fun sendCallbackToNative(rnCallback: Callback) {
-        rnCallback.invoke("A greeting from java")
-    }
+  @ReactMethod
+  fun sendCallbackToNative(rnCallback: Callback) {
+    rnCallback.invoke("A greeting from java")
+  }
 
-    @ReactMethod
-    fun finishActivity() {
-        if (currentActivity != null) {
-            currentActivity!!.finish()
-        }
-    }
+  @ReactMethod
+  fun finishActivity() {
+    currentActivity?.finish()
+  }
 
-    @ReactMethod
-    fun goToSecondActivity() {
-        if (currentActivity != null) {
-            val intent = Intent(currentActivity, SecondActivity::class.java)
-            currentActivity!!.startActivity(intent)
-        }
-    }
+  @ReactMethod
+  fun goToSecondActivity() {
+    val intent = Intent(currentActivity, SecondActivity::class.java)
+    currentActivity?.startActivity(intent)
+  }
 
-    companion object
+  companion object
 
 }
